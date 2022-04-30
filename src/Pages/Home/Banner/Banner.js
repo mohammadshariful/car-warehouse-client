79971593beaa0fcac0108204743e40dd77b72186
mohammadshariful <mohammadsharifulislam64@gmail.com>
2022-04-30 12:21:57 +0600

    typing animation added

--- src/Pages/Home/Banner/Banner.js
+++ src/Pages/Home/Banner/Banner.js
@@ -1,12 +1,18 @@
 import React from "react";
 import { FaAngleRight } from "react-icons/fa";
+import TypeAnimation from "react-type-animation";
 import bannerImg from "../../../Assets/Images/bannerImg.png";
 import "./Banner.css";
 const Banner = () => {
   return (
     <div className="banner-container">
       <div className="banner-info">
-        <h1>Lets geeting a new car !!</h1>
+        <TypeAnimation
+          cursor={true}
+          sequence={["Lets geeting a new car.", 2000, ""]}
+          wrapper="h1"
+          repeat={Infinity}
+        />
         <p>
           Your dream is here.Choose your favourite car and buy it.we have
           provided our best services.
