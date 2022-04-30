0c19104a9c7d3175cd367c20201039cf87f8a047
mohammadshariful <mohammadsharifulislam64@gmail.com>
2022-04-30 22:53:30 +0600

    user created with firebase

--- src/Pages/SignUp/SingUp.js
+++ src/Pages/SignUp/SingUp.js
@@ -1,11 +1,46 @@
-import React from "react";
+import React, { useEffect } from "react";
 import { Container, Form } from "react-bootstrap";
+import { useCreateUserWithEmailAndPassword } from "react-firebase-hooks/auth";
 import { FaUser } from "react-icons/fa";
 import { Link } from "react-router-dom";
 import singupImg from "../../Assets/Images/linear.jpg";
+import auth from "../../Firebase/Firebase.init";
+import useStateHandle from "../../Hooks/useStateHandle";
 import SocialLogin from "../Shared/SocialLogin/SocialLogin";
 import "./SingUp.css";
 const SingUp = () => {
+  const {
+    email,
+    password,
+    confirmPassword,
+    handleName,
+    handleEmail,
+    handlePassword,
+    handleConfirmPassword,
+  } = useStateHandle();
+  const [createUserWithEmailAndPassword, user, , error] =
+    useCreateUserWithEmailAndPassword(auth, { sendEmailVerification: true });
+
+  useEffect(() => {
+    if (user) {
+      console.log("toiri hoice");
+    }
+  }, [user]);
+
+  const submitHandle = (event) => {
+    event.preventDefault();
+    const emailValue = email.value;
+    const passwordValue = password.value;
+    const confirmPasswordValue = confirmPassword.value;
+    if (passwordValue === confirmPasswordValue) {
+      createUserWithEmailAndPassword(emailValue, passwordValue);
+    }
+  };
+
+  if (error) {
+    console.log(error);
+  }
+
   return (
     <Container className=" d-flex justify-content-center  my-5">
       <div className="submit-area w-75  mx-auto">
@@ -17,26 +52,48 @@ const SingUp = () => {
             <FaUser className="user-icon" />
           </div>
           <h2 className="section-title text-center">SignUp</h2>
-          <Form className="w-75 mx-auto">
+          <Form onSubmit={submitHandle} className="w-75 mx-auto">
             <Form.Group className="mb-2" controlId="name">
               <Form.Label>Name</Form.Label>
-              <Form.Control type="text" placeholder="Enter Name" />
+              <Form.Control
+                onBlur={handleName}
+                type="text"
+                placeholder="Enter Name"
+                required
+              />
             </Form.Group>
             <Form.Group className="mb-2" controlId="email">
               <Form.Label>Email address</Form.Label>
-              <Form.Control type="email" placeholder="Enter email" />
+              <Form.Control
+                onBlur={handleEmail}
+                type="email"
+                placeholder="Enter email"
+                required
+              />
+              {email?.error && <p className="error">{email.error}</p>}
             </Form.Group>
             <Form.Group className="mb-2" controlId="password">
               <Form.Label>Password</Form.Label>
-              <Form.Control type="password" placeholder="Enter password" />
+              <Form.Control
+                onBlur={handlePassword}
+                type="password"
+                placeholder="Enter password"
+                required
+              />
             </Form.Group>
+            {password?.error && <p className="error">{password.error}</p>}
             <Form.Group className="mb-2" controlId="confirmPassword">
               <Form.Label>Confirm Password</Form.Label>
               <Form.Control
+                onBlur={handleConfirmPassword}
                 type="password"
                 placeholder="Enter Confirm Password"
+                required
               />
             </Form.Group>
+            {confirmPassword?.error && (
+              <p className="error">{confirmPassword.error}</p>
+            )}
             <button className="submit-btn" type="submit">
               SignUp
             </button>
